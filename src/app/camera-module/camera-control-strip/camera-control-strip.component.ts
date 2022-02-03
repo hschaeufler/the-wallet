84edab0e1969d910180a1f0da93af3f2f1e8e729
Holger Schäufler <hlsch@gmx.net>
2022-02-03 01:30:25 +0100

    Create camera-control-strip

---
+++ src/app/camera-module/camera-control-strip/camera-control-strip.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-control-strip',
+  template: `
+    <p>
+      camera-control-strip works!
+    </p>
+  `,
+  styleUrls: ['./camera-control-strip.component.scss']
+})
+export class CameraControlStripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/camera-module/camera.module.ts
+++ src/app/camera-module/camera.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CameraComponent } from "./camera/camera.component";
+import { CameraControlStripComponent } from './camera-control-strip/camera-control-strip.component';
 
 
 @NgModule({
   declarations: [
     CameraComponent,
+    CameraControlStripComponent,
   ],
   imports: [
     CommonModule,
