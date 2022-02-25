dbea024159b02c9c369339e54ad2ce2063f3794e
Holger Schäufler <hlsch@gmx.net>
2022-02-25 03:54:21 +0100

    Remove unused import

--- src/app/camera-module/camera-dialog/camera-dialog.component.ts
+++ src/app/camera-module/camera-dialog/camera-dialog.component.ts
@@ -1,12 +1,11 @@
 import {Component, ViewEncapsulation} from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'the-wallet-camera-dialog',
   template: `
       <mat-toolbar class="camera-dialog-component-title" color="primary">
         <mat-toolbar-row class="camera-dialog-component-title-row">
-          <h1 mat-dialog-title>Camera!</h1>
+          <h1 mat-dialog-title>Camera</h1>
           <button class="close-button" mat-dialog-close>
             <mat-icon>close</mat-icon>
           </button>
