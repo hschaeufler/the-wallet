b04179f7387606d1e464dcf25991a490a7908cc2
Holger Schäufler <hlsch@gmx.net>
2022-03-05 18:20:06 +0100

    Implement ReadFile-Method

--- src/app/modules/file-system/services/file-system.service.ts
+++ src/app/modules/file-system/services/file-system.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { concatMap, from, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileSystemService {
   constructor() {}
+
+  readfile(options?: FilePickerOptions): Observable<Blob> {
+    return from(window.showOpenFilePicker(options)).pipe(
+      concatMap(([fileHandle]) => from(fileHandle.getFile())),
+      concatMap((file) => from(file.arrayBuffer())),
+      map((arrayBuffer) => new Blob([arrayBuffer]))
+    );
+  }
 }
