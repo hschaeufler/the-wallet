cf7f9b6e9592f1f3c247e098ed390494ecb5d227
Holger Schäufler <hlsch@gmx.net>
2022-03-07 21:43:31 +0100

    Implement dom-File-Download

--- src/app/modules/file-system/services/dom-file-api.service.ts
+++ src/app/modules/file-system/services/dom-file-api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FileSystemService } from './FileSystem.service';
-import { delay, fromEvent, map, Observable, race } from 'rxjs';
+import { delay, fromEvent, map, Observable, race, throwError } from 'rxjs';
 import { filePickerAcceptTypesToAcceptString } from './dom-file-api.utils';
+import { blobToDataURL } from '../../commons/utils/image-conversion.utils';
 
 @Injectable({
   providedIn: 'root',
@@ -9,48 +10,59 @@ import { filePickerAcceptTypesToAcceptString } from './dom-file-api.utils';
 export class DomFileApiService implements FileSystemService {
   constructor() {}
 
-  writeFile(
-    file: File,
-    suggestedName?: string,
-    types?: FilePickerAcceptType[],
-    excludeAcceptAllOption?: boolean
-  ): Observable<void> {
-    throw new Error('Method not implemented.');
+  writeFile(file: File, suggestedName?: string): Observable<void> {
+    try {
+      const aElement = document.createElement('a');
+      return blobToDataURL(file).pipe(
+        map((dataURL) => {
+          aElement.href = dataURL;
+          aElement.download = suggestedName || '';
+          aElement.click();
+          return;
+        })
+      );
+    } catch (e) {
+      return throwError(() => e);
+    }
   }
 
   readFiles(
     types?: FilePickerAcceptType[],
     allowMultipleFiles?: boolean
   ): Observable<File[]> {
-    const inputElement = document.createElement('input');
-    inputElement.type = 'file';
-    inputElement.multiple = allowMultipleFiles || false;
-    inputElement.accept = types
-      ? filePickerAcceptTypesToAcceptString(types)
-      : '';
-    const blob$ = race(
-      fromEvent(inputElement, 'change').pipe(
-        map(() => {
-          const fileList = inputElement.files;
-          if (!fileList || fileList.length < 1) {
+    try {
+      const inputElement = document.createElement('input');
+      inputElement.type = 'file';
+      inputElement.multiple = allowMultipleFiles || false;
+      inputElement.accept = types
+        ? filePickerAcceptTypesToAcceptString(types)
+        : '';
+      const blob$ = race(
+        fromEvent(inputElement, 'change').pipe(
+          map(() => {
+            const fileList = inputElement.files;
+            if (!fileList || fileList.length < 1) {
+              throw new DOMException('No File selected!');
+            }
+            return Array.from(fileList);
+          })
+        ),
+        /*
+         * Firefox does not send a change event when the file picker is closed.
+         * If 1000 miliseconds after firing the focusin event the change event was not fired,
+         * we assume that the user did not make a file selection.
+         * */
+        fromEvent(window, 'focusin').pipe(
+          delay(1000),
+          map((event) => {
             throw new DOMException('No File selected!');
-          }
-          return Array.from(fileList);
-        })
-      ),
-      /*
-       * Firefox does not send a change event when the file picker is closed.
-       * If 1000 miliseconds after firing the focusin event the change event was not fired,
-       * we assume that the user did not make a file selection.
-       * */
-      fromEvent(window, 'focusin').pipe(
-        delay(1000),
-        map((event) => {
-          throw new DOMException('No File selected!');
-        })
-      )
-    );
-    inputElement.click();
-    return blob$;
+          })
+        )
+      );
+      inputElement.click();
+      return blob$;
+    } catch (e) {
+      return throwError(() => e);
+    }
   }
 }
