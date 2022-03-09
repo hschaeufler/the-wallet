ada61c07a7fac21ee03b54b4dea93fed68b32002
Holger Schäufler <hlsch@gmx.net>
2022-03-09 23:12:43 +0100

    Add Brackpoint-Handling to Main-Theme

--- src/app/components/templates/page-template/page-template.component.ts
+++ src/app/components/templates/page-template/page-template.component.ts
@@ -1,20 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'the-wallet-page-template',
   template: `
     <ng-content select="[theWalletPageTemplateHeader]"></ng-content>
     <div class="container">
-      <main class="main">
+      <main [class]="isLargeScreen ? 'main-large' : 'main'">
         <ng-content></ng-content>
       </main>
     </div>
   `,
   styleUrls: ['./page-template.component.scss'],
 })
-export class PageTemplateComponent {
+export class PageTemplateComponent implements OnInit, OnDestroy {
   @Input()
   title: string = '';
 
-  constructor() {}
+  private beakpointSubscription$?: Subscription;
+  isLargeScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.beakpointSubscription$ = this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((result) => {
+        console.log(result);
+        this.isLargeScreen = !result.matches;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.beakpointSubscription$) {
+      this.beakpointSubscription$.unsubscribe();
+    }
+  }
 }
