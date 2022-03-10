be7e188c8780d52783f3d3f989ce860f488dc1e4
Holger Schäufler <hlsch@gmx.net>
2022-03-10 04:20:35 +0100

    Refactory Recovery and Testdetail

--- src/app/modules/health-certificate/components/molecules/test-detail/test-detail.component.ts
+++ src/app/modules/health-certificate/components/molecules/test-detail/test-detail.component.ts
@@ -4,23 +4,40 @@ import { TestEntry } from 'covid-certificate-checker/dist/lib/models/HealthCerti
 @Component({
   selector: 'the-wallet-test-detail',
   template: `
-    <the-wallet-default-expansion-panel title="Test">
+    <mat-list>
+      <mat-divider></mat-divider>
+      <mat-list-item>
+        <p mat-line>Test Result</p>
+        <h1 mat-line>{{ value.tr | toTestResult }}</h1>
+      </mat-list-item>
+      <mat-divider></mat-divider>
+      <mat-list-item>
+        <p mat-line>Test Type</p>
+        <h1 mat-line>{{ value.tt | testType }}</h1>
+      </mat-list-item>
+      <mat-divider></mat-divider>
+      <mat-list-item>
+        <p mat-line>Sample Collection Date</p>
+        <h1 mat-line>{{ value.sc | toLocaleDate }}</h1>
+      </mat-list-item>
+      <mat-divider></mat-divider>
+      <mat-list-item>
+        <p mat-line>Target Disease</p>
+        <h1 mat-line>{{ value.tg | targetDisease }}</h1>
+      </mat-list-item>
+      <mat-divider></mat-divider>
+      <mat-list-item *ngIf="value.nm">
+        <p mat-line>Test Name</p>
+        <h1 mat-line>{{ value.nm }}</h1>
+      </mat-list-item>
+      <mat-divider></mat-divider>
+      <mat-list-item>
+        <p mat-line>Testing Center</p>
+        <h1 mat-line>{{ value.tc }}</h1>
+      </mat-list-item>
+    </mat-list>
+    <the-wallet-default-expansion-panel title="Details">
       <mat-list>
-        <mat-divider></mat-divider>
-        <mat-list-item>
-          <p mat-line>Target Disease</p>
-          <h1 mat-line>{{ value.tg | targetDisease }}</h1>
-        </mat-list-item>
-        <mat-divider></mat-divider>
-        <mat-list-item>
-          <p mat-line>Test Type</p>
-          <h1 mat-line>{{ value.tt | testType }}</h1>
-        </mat-list-item>
-        <mat-divider></mat-divider>
-        <mat-list-item *ngIf="value.nm">
-          <p mat-line>Test Name</p>
-          <h1 mat-line>{{ value.nm }}</h1>
-        </mat-list-item>
         <mat-divider></mat-divider>
         <mat-list-item *ngIf="value.ma">
           <p mat-line>Manufacturer</p>
@@ -28,22 +45,7 @@ import { TestEntry } from 'covid-certificate-checker/dist/lib/models/HealthCerti
         </mat-list-item>
         <mat-divider></mat-divider>
         <mat-list-item>
-          <p mat-line>Sample Collection Date</p>
-          <h1 mat-line>{{ value.sc | toLocaleDate }}</h1>
-        </mat-list-item>
-        <mat-divider></mat-divider>
-        <mat-list-item>
-          <p mat-line>Test Result</p>
-          <h1 mat-line>{{ value.tr | toTestResult }}</h1>
-        </mat-list-item>
-        <mat-divider></mat-divider>
-        <mat-list-item>
-          <p mat-line>Testing Center</p>
-          <h1 mat-line>{{ value.tc }}</h1>
-        </mat-list-item>
-        <mat-divider></mat-divider>
-        <mat-list-item>
-          <p mat-line>Country of Vaccination</p>
+          <p mat-line>Country of Test</p>
           <h1 mat-line>{{ value.co | toCountryName }}</h1>
         </mat-list-item>
         <mat-divider></mat-divider>
