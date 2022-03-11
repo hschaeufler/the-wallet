07b014778022cf60965df2d370380fbb501e22eb
Holger Schäufler <hlsch@gmx.net>
2022-03-11 16:00:58 +0100

    Update Base-Href for DSC-List

--- src/app/modules/health-certificate/services/document-signer-certificate-service.service.ts
+++ src/app/modules/health-certificate/services/document-signer-certificate-service.service.ts
@@ -4,25 +4,29 @@ import { map, Observable } from 'rxjs';
 import { TrustListModel } from 'covid-certificate-checker/dist/lib/models/TrustList.model';
 import { Router } from '@angular/router';
 import { environment } from '../../../../environments/environment';
+import { PlatformLocation } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentSignerCertificateServiceService {
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private platformLocation: PlatformLocation
+  ) {}
 
   getDSCList(): Observable<TrustListModel> {
-    return this.http
-      .get(environment.trustListURL, { responseType: 'text' })
-      .pipe(
-        map((result) => {
-          const certificates = result.match(/({\"certificates\")[\s\S]*$/);
-          if (certificates && certificates.length > 0) {
-            return JSON.parse(certificates[0]) as TrustListModel;
-          } else {
-            throw new DOMException('No valid DocumentSignerCertificateList');
-          }
-        })
-      );
+    const trustList =
+      this.platformLocation.getBaseHrefFromDOM() + environment.trustListURL;
+    return this.http.get(trustList, { responseType: 'text' }).pipe(
+      map((result) => {
+        const certificates = result.match(/({\"certificates\")[\s\S]*$/);
+        if (certificates && certificates.length > 0) {
+          return JSON.parse(certificates[0]) as TrustListModel;
+        } else {
+          throw new DOMException('No valid DocumentSignerCertificateList');
+        }
+      })
+    );
   }
 }
