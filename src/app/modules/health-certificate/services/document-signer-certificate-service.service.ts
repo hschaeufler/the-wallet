40af22c0cbf66b7b9f850dc6632841bc47db4414
Holger Schäufler <hlsch@gmx.net>
2022-03-11 14:51:52 +0100

    Add Verification with Trust-List

--- src/app/modules/health-certificate/services/document-signer-certificate-service.service.ts
+++ src/app/modules/health-certificate/services/document-signer-certificate-service.service.ts
@@ -2,25 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { TrustListModel } from 'covid-certificate-checker/dist/lib/models/TrustList.model';
+import { Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentSignerCertificateServiceService {
-  configURL = 'https://de.test.dscg.ubirch.com/trustList/DSC/';
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getDSCList(): Observable<TrustListModel> {
-    return this.http.get(this.configURL, { responseType: 'text' }).pipe(
-      map((result) => {
-        const certificates = result.match(/({\"certificates\")[\s\S]*$/);
-        if (certificates && certificates.length > 0) {
-          return JSON.parse(certificates[0]) as TrustListModel;
-        } else {
-          throw new DOMException('No valid DocumentSignerCertificateList');
-        }
-      })
-    );
+    return this.http
+      .get(environment.trustListURL, { responseType: 'text' })
+      .pipe(
+        map((result) => {
+          const certificates = result.match(/({\"certificates\")[\s\S]*$/);
+          if (certificates && certificates.length > 0) {
+            return JSON.parse(certificates[0]) as TrustListModel;
+          } else {
+            throw new DOMException('No valid DocumentSignerCertificateList');
+          }
+        })
+      );
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  trustListURL : '/assets/trustlist/DSC.json',
 };
